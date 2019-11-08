888e15ec5db7934823ad2cb20c4bd9e070afd120
David Cha <david.cha44@qmail.cuny.edu>
2019-11-08 19:27:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,7 +18,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min = 0
+  min = 1000000000000000
   array.length.times do |count|
     if array[count] < min
       min = array[count]
